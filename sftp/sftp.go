aa00b289f640bc27aa5f1811cef89a8046bb0419
Emilio Mendoza <emendoza@uwaterloo.ca>
2026-08-07 20:07:03 -0400

    sftp: enable sftp server on startup and wiring for other protocols
    Signed-off-by: Emilio Mendoza <emendoza@uwaterloo.ca>

--- sftp/sftp.go
+++ sftp/sftp.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
@@ -16,7 +17,7 @@ import (
 	"tailscale.com/tsnet"
 )
 
-func Run() {
+func Run(ctx context.Context) error {
 	srv := &tsnet.Server{
 		Hostname: "sftp-vfs",
 		// AuthKey:  // os.Getenv("TS_AUTHKEY"),
@@ -25,7 +26,7 @@ func Run() {
 
 	lc, err := srv.LocalClient()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	sshConfig := &ssh.ServerConfig{
@@ -34,30 +35,50 @@ func Run() {
 
 	hostKey, err := loadOrCreateHostKey("host_key")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	sshConfig.AddHostKey(hostKey)
 
 	ln, err := srv.Listen("tcp", ":22")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	defer ln.Close()
+
+	// Unblocks the Accept below when the registry cancels us.
+	go func() {
+		<-ctx.Done()
+		ln.Close()
+	}()
 
 	log.Println("sftp-vfs listening on the tailnet :22")
 
+	var conns sync.WaitGroup
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
+			if ctx.Err() != nil {
+				break
+			}
 			continue
 		}
-		go handleConn(conn, sshConfig, lc)
+
+		conns.Add(1)
+		go func() {
+			defer conns.Done()
+			handleConn(ctx, conn, sshConfig, lc)
+		}()
 	}
+
+	conns.Wait()
+	log.Println("sftp-vfs stopped")
+	return ctx.Err()
 }
 
-func handleConn(nConn net.Conn, sshConfig *ssh.ServerConfig, lc *local.Client) {
+func handleConn(ctx context.Context, nConn net.Conn, sshConfig *ssh.ServerConfig, lc *local.Client) {
 	login := "anonymous"
-	if who, err := lc.WhoIs(context.Background(), nConn.RemoteAddr().String()); err == nil {
+	if who, err := lc.WhoIs(ctx, nConn.RemoteAddr().String()); err == nil {
 		login = who.UserProfile.LoginName
 	}
 
@@ -70,6 +91,18 @@ func handleConn(nConn net.Conn, sshConfig *ssh.ServerConfig, lc *local.Client) {
 
 	defer sconn.Close()
 	go ssh.DiscardRequests(reqs)
+
+	// Tears down the session on cancellation; the derived context keeps this
+	// goroutine from outliving the connection.
+	connCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	go func() {
+		<-connCtx.Done()
+		sconn.Close()
+	}()
+
+	var sessions sync.WaitGroup
+	defer sessions.Wait()
 
 	for newChan := range chans {
 		if newChan.ChannelType() != "session" {
@@ -98,7 +131,9 @@ func handleConn(nConn net.Conn, sshConfig *ssh.ServerConfig, lc *local.Client) {
 			FileList: fs,
 		})
 
+		sessions.Add(1)
 		go func() {
+			defer sessions.Done()
 			if err := server.Serve(); err != nil && err != io.EOF {
 				log.Println("sftp serve:", err)
 			}
